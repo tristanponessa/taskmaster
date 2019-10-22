bb663f6d6814aaa3feba6f4dabda29a469bef887
Tristan PONESSA <trponess@e1r7p7.42.fr>
2019-10-22 06:27:31 +0200

    2nd save

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 #    By: trponess <trponess@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/10/22 00:21:18 by trponess          #+#    #+#              #
-#    Updated: 2019/10/22 06:01:38 by trponess         ###   ########.fr        #
+#    Updated: 2019/10/22 06:24:16 by trponess         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,6 +16,7 @@ import time
 import sys
 from cmd import Cmd
 
+"""
 class _Wrapper:
 
     def __init__(self, fd):
@@ -37,8 +38,7 @@ class MyPrompt(Cmd):
         super().__init__(stdin=_Wrapper(sys.stdin))
         self.use_rawinput = False
         self.prompt = '$ '
-
-    FRIENDS = [ 'Alice', 'Adam', 'Barbara', 'Bob' ]
+        self.FRIENDS = [ 'Alice', 'Adam', 'Barbara', 'Bob' ]
     
     def do_greet(self, person):
         "Greet the person"
@@ -95,5 +95,44 @@ class MyPrompt(Cmd):
  #   help_EOF = help_exit
 
 
+
 if __name__ == '__main__':
     MyPrompt().cmdloop()
+"""
+
+import cmd
+
+class MyCmd(cmd.Cmd):
+
+
+    addresses = [
+'here@blubb.com',
+'foo@bar.com',
+'whatever@wherever.org']
+
+def do_send(self, line):
+    "Greet the person"
+    if line and line in self.addresses:
+        sending_to = 'Sending to, %s!' % line
+    elif line:
+        sending_to = "Send to, " + line + " ?"
+    else:
+        sending_to = 'noreply@example.com'
+    print (sending_to)
+
+def complete_send(self, text, line, begidx, endidx):
+    if text:
+        completions = [
+            address for address in self.addresses
+            if address.startswith(text)
+        ]
+    else:
+        completions = self.addresses[:]
+
+    return completions
+
+def do_EOF(self, line):
+    return True
+
+if __name__ == '__main__':
+    MyCmd().cmdloop()
