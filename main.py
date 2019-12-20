a4e4b8f2dfcc5b76783fa5a09e3a949a869bb48b
tristanponessa <tristanponessa@hotmail.com>
2019-12-20 03:58:15 +0100

    big update

--- main.py
+++ main.py
@@ -1,80 +1,53 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    main.py                                            :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: trponess <trponess@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2019/10/22 00:21:18 by trponess          #+#    #+#              #
-#    Updated: 2019/10/24 14:11:22 by trponess         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
 
-#!/usr/bin/python3.6
+import cmd
+import subprocess
 
-import time
-import sys
-from cmd import Cmd
-import keyword
-
-
-class _Wrapper:
-
-    linex = None
+class Taskmaster_shell(cmd.Cmd):
     
-    def __init__(self, fd):
-        self.fd = fd
-        
-    def readline(self, *args):
-        try:
-            line = self.fd.readline(*args)
-            #print("<", line.split(" "), ">")
-            #self.linex = line
-            return line
-        except KeyboardInterrupt:#ctrl+C
-            print("\nKeyboardInterrupt : ctrl+C")
-            return ('\n')
-         
-
-class MyPrompt(Cmd):
     prompt = '$> '
     intro = "Welcome! Type ? to list commands"
-
+    
+    FRIENDS = [ 'Alice', 'Adam', 'Barbara', 'Bob' ]
+    
     def __init__(self):
-        self.use_rawinput = False
-        super().__init__(stdin=_Wrapper(sys.stdin))
+        super().__init__()
+        self.data = dict()
+        with open("config/.conf", "r") as f:
+            lines = f.readlines()
+        for line in lines:
+            key, value = line.split('=')
+            self.data[key] = value
+        
+        #create file
+        open(self.data['stdout'], "a+").close()
+        open(self.data['stderr'], "a+").close()
 
     def emptyline(self):
         pass
+    
+    def default(self, inp):
+        print("command don't exist")
       
     def do_exit(self, inp):
         print("Bye")
         return True
     
-    def do_add(self, inp):
-        print("adding '{}'".format(inp))
-
-    def help_add(self):
-        print("Add a new entry to the system.")
-
-    def help_exit(self):
-        print('exit the application. Shorthand: x q Ctrl-D.')
-
+    def do_run(self, person):
+        with open(self.data['stdout'], "w") as stdout_file, \
+             open(self.data['stderr'], "w") as stderr_file:
+            subprocess.run(["python3", "time_program.py"], stdout=stdout_file ,stderr=stderr_file)
+        print("runnung time")
     
-    def do_EOF(self, inp):
-        print("EOF : ctrl+D")
-        return self.do_exit(inp)
-        
-    def default(self, inp):
-        print("command don't exist")
-            
-
- 
-#    do_EOF = do_exit
- #   help_EOF = help_exit
-
-
+    def complete_greet(self, text, line, begidx, endidx):
+        if not text:
+            completions = self.FRIENDS[:]
+        else:
+            completions = [f for f in self.FRIENDS if f.startswith(text)]
+        return completions
+    
+    def do_EOF(self, line):
+        return True
 
 if __name__ == '__main__':
-    MyPrompt().cmdloop()
-
+    ts = Taskmaster_shell()
+    ts.cmdloop()
--- pro_forever.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3.8
-
-import time
-
-i = 0
-while True:
-    print("loop:", i)
-    time.sleep(2)
-    i += 1  
